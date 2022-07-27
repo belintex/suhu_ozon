2d5532e143464f339b7914356b04095d91a44bbc
Belintex <belintex8@gmail.com>
2022-07-28 06:33:31 +0700

    koneksi mysqli

--- resources/views/layouts/bacafan.blade.php
+++ resources/views/layouts/bacafan.blade.php
@@ -1,5 +1,5 @@
 <?php
-    $konek = mysqli_connect('localhost', 'root', '', 'auth');
+    $konek = mysqli_connect('localhost', env('DB_USERNAME'), env('DB_PASSWORD'), env('DB_DATABASE'));
 
     $sql = mysqli_query($konek, "SELECT * FROM switchings");
     $data = mysqli_fetch_array($sql);
